a801ea7caeb5fa25e49b81d4c2169b87268db7a7
Peter John Dones <Peterjohndones112@gmail.com>
2025-11-04 09:17:43 +0800

    added script tag in format blade

--- lms_webdev/resources/views/components/format.blade.php
+++ lms_webdev/resources/views/components/format.blade.php
@@ -16,6 +16,36 @@
     <!-- para sa icons iconify na since ayun din gamit sa figma or dagdag kayo here -->
     <script src="https://code.iconify.design/iconify-icon/2.1.0/iconify-icon.min.js"></script>
 
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        pastel: {
+                            pink: '#FFD6EA',
+                            yellow: '#FFE6A6',
+                            blue: '#D1E4E2',
+                            purple: '#D7CAE8',
+                        },
+                        main: '#000000',
+                        page: '#FAF8F5',
+                    },
+                    fontFamily: {
+                        outfit: ['Outfit', 'sans-serif'],
+                        poppins: ['Poppins', 'sans-serif'],
+                    },
+                    boxShadow: {
+                        'pastel-pink': '4px 4px 0 #FFD6EA',
+                        'pastel-yellow': '4px 4px 0 #FFE6A6',
+                        'pastel-blue': '4px 4px 0 #D1E4E2',
+                        'pastel-purple': '4px 4px 0 #D7CAE8',
+                        'main': '4px 4px 0 #000000',
+                    },
+                },
+            },
+        };
+    </script>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
